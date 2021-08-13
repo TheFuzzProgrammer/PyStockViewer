0986f5ba9dc10e29fe0f59fed4b68d736b633d58
Fuzz <raulcarrasco654@gmail.com>
2021-08-13 19:14:06 -0300

    Added some modifications.
    Compatibility w local reg.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from modules.objectMaker import *
+from modules.dumper import *
+from modules.people import *
+
+liquid = Liquid(7, 2.5, "pepsi", 2, 10)
+
+liquid.set_expiration(True, "12/21")
+
+liquid.is_food(True)
+
+dump_object(liquid)
 
--- modules/dumper.py
+++ modules/dumper.py
@@ -1,31 +1,33 @@
 __author__ = "Fuzz"
 
 import pickle
-from objectMaker import *
-from people import *
+from modules.objectMaker import *
+from modules.people import *
 
 
 def dump_object(object_to_dump):
-    if type(object) is Product:
+    if type(object_to_dump) is Liquid:
+        print(object_to_dump.isFood)
         if object_to_dump.isFood:
-            file_db = 'food.dat'
+            file_db = 'Data/Food.dat'
             data = open(file_db, 'ab')
             pickle.dump(object_to_dump, data)
             data.close()
         else:
-            file_db = 'products.dat'
+            file_db = 'Data/Products.dat'
             data = open(file_db, 'ab')
             pickle.dump(object_to_dump, data)
             data.close()
+
     elif type(object_to_dump) is Person:
         if type(object_to_dump) is Client:
-            file_db = 'clients.dat'
+            file_db = 'Data/Clients.dat'
             data = open(file_db, 'ab')
             pickle.dump(object_to_dump, data)
             data.close()
 
         elif type(object_to_dump) is Employed:
-            file_db = 'partners.dat'
+            file_db = 'Data/Partners.dat'
             data = open(file_db, 'ab')
             pickle.dump(object_to_dump, data)
             data.close()
