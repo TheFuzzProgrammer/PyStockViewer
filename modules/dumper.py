0b15542d15ac074c8551bf73e33c3bd564b09046
Fuzz <raulcarrasco654@gmail.com>
2021-08-13 03:03:31 -0300

    Update dumper.py

--- modules/dumper.py
+++ modules/dumper.py
@@ -4,29 +4,30 @@ import pickle
 
 from objectMaker import *
 
-def dump_object(object):
+
+def dump_object(object_to_dump):
     if type(object) is Product:
-        if object.isFood:
+        if object_to_dump.isFood:
             file_db = 'food.dat'
             data = open(file_db, 'ab')
-            pickle.dump(object, data)
+            pickle.dump(object_to_dump, data)
             data.close()
         else:
             file_db = 'products.dat'
             data = open(file_db, 'ab')
-            pickle.dump(object, data)
+            pickle.dump(object_to_dump, data)
             data.close()
-    elif type(object) is Person:
-        if type(object) is Client:
+    elif type(object_to_dump) is Person:
+        if type(object_to_dump) is Client:
             file_db = 'clients.dat'
             data = open(file_db, 'ab')
-            pickle.dump(object, data)
+            pickle.dump(object_to_dump, data)
             data.close()
 
-        elif type(object) is Employed:
+        elif type(object_to_dump) is Employed:
             file_db = 'partners.dat'
             data = open(file_db, 'ab')
-            pickle.dump(object, data)
+            pickle.dump(object_to_dump, data)
             data.close()
         else:
             pass
