8f02fff7c704af4cd0dd0d04abd020c635100b54
Fuzz <raulcarrasco654@gmail.com>
2021-08-13 02:17:19 -0300

    Update dumper.py

--- modules/dumper.py
+++ modules/dumper.py
@@ -6,6 +6,29 @@ import objectMaker
 
 def dump_object(object):
     if type(object) is Product:
+        if object.isFood:
+            file_db = 'food.dat'
+            data = open(file_db, 'ab')
+            pickle.dump(object, data)
+            data.close()
+        else:
+            file_db = 'products.dat'
+            data = open(file_db, 'ab')
+            pickle.dump(object, data)
+            data.close()
+    elif type(object) is Person:
+        if type(object) is Client:
+            file_db = 'clients.dat'
+            data = open(file_db, 'ab')
+            pickle.dump(object, data)
+            data.close()
 
-
-
+        elif type(object) is Employed:
+            file_db = 'partners.dat'
+            data = open(file_db, 'ab')
+            pickle.dump(object, data)
+            data.close()
+        else:
+            pass
+    else:
+        pass
