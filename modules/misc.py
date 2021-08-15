7c918bd9c37c6ff7ab0791db4d4a6095e60a909f
Fuzz <raulcarrasco654@gmail.com>
2021-08-15 18:12:12 -0300

    Create misc.py
    contains all impositive classes info

---
+++ modules/misc.py
@@ -0,0 +1,27 @@
+__Author__ = "Fuzz"
+
+
+class Tax:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.active = True
+
+    def is_active(self, status):
+        self.active = status
+
+    def change_value(self, value):
+        self.value = value
+
+
+class Discount(Tax):
+    def __init__(self, name, value, reason):
+        super().__init__(name, value)
+        self.name = name
+        self.value = (-1) * value
+        self.active = True
+        self.reason = reason
+
+
+class 
+
